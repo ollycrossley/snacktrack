d1e580c2d493f93a25ad06544793ea67e66b6a9a
Oliver Crossley <ollythecrossley@outlook.com>
2023-09-19 10:46:42 +0100

    adjusts naming of keys in Reviews

--- back-end/schemas/Reviews.js
+++ back-end/schemas/Reviews.js
@@ -1,1 +1,28 @@
-import mongoose from "mongoose";
+import {Schema, SchemaTypes, model} from "mongoose";
+
+const ReviewSchema = new Schema({
+
+    created_at: {
+        type: Date,
+        default: Date.now()
+    },
+    body: String,
+    rating: {
+        type: Number,
+        required: true
+    },
+    customer: {
+        type: SchemaTypes.ObjectId,
+        ref: "Customer",
+        required: true,
+    },
+    business: {
+        type: SchemaTypes.ObjectId,
+        ref: "Business",
+        required: true,
+    },
+
+})
+
+const Review = model("Review", ReviewSchema )
+module.exports = Review
