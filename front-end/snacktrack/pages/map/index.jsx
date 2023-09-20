fbf3f944b1cb86e6413d8306ed8c3b25ddc56363
danny-817 <d_halsall@hotmail.com>
2023-09-20 16:40:11 +0100

    added create driver and customer pages. both pages check the password.length !==0 and the matches the passwordConfirm

---
+++ front-end/snacktrack/pages/map/index.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../navbar";
+import SimpleMap from "./components/simplemap";
+
+export default function Map() {
+  return (
+    <>
+      <NavBar />
+      <h1>MAP</h1>
+      <SimpleMap />
+    </>
+  );
+}
