d7eacbb70b31bd5fd18f56e7727a4737f173005c
Oliver Crossley <ollythecrossley@outlook.com>
2023-09-21 10:57:35 +0100

    adds pins to the map

--- front-end/snacktrack/pages/map/index.jsx
+++ front-end/snacktrack/pages/map/index.jsx
@@ -1,9 +1,13 @@
 import NavBar from "../navbar";
 import SimpleMap from "./components/simplemap";
+import Head from "next/head";
 
 export default function Map() {
   return (
     <>
+        <Head>
+            <script src="https://kit.fontawesome.com/a1a2d1f110.js" crossOrigin="anonymous"></script>
+        </Head>
       <NavBar />
       <h1>MAP</h1>
       <SimpleMap />
