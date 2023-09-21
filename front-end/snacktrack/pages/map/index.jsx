a8ab935232f081b934495f96e859c72039ad3009
Oliver Crossley <ollythecrossley@outlook.com>
2023-09-21 15:13:53 +0100

    fixes script error

--- front-end/snacktrack/pages/map/index.jsx
+++ front-end/snacktrack/pages/map/index.jsx
@@ -1,15 +1,13 @@
 import NavBar from "../navbar";
 import SimpleMap from "./components/simplemap";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function Map() {
   return (
     <>
       <Head>
-        <script
-          src="https://kit.fontawesome.com/a1a2d1f110.js"
-          crossOrigin="anonymous"
-        ></script>
+          <Script src={"https://kit.fontawesome.com/a1a2d1f110.js"} crossOrigin="anonymous" />
       </Head>
       <NavBar />
       <h1>MAP</h1>
