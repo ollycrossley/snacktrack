7fdb89302d4c37740e93f24397f07c7ec171e463
Oliver Crossley <ollythecrossley@outlook.com>
2023-09-21 15:29:45 +0100

    prepares light styling for the map

--- front-end/snacktrack/pages/map/index.jsx
+++ front-end/snacktrack/pages/map/index.jsx
@@ -7,11 +7,14 @@ export default function Map() {
   return (
     <>
       <Head>
-          <Script src={"https://kit.fontawesome.com/a1a2d1f110.js"} crossOrigin="anonymous" />
+        <script
+          src="https://kit.fontawesome.com/a1a2d1f110.js"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <NavBar />
-      <h1>MAP</h1>
-      <SimpleMap />
+      <h1 className={"title has-text-centered"}>MAP</h1>
+        <div className={"container"} style={{marginBottom: 25}}><SimpleMap/></div>
     </>
   );
 }
