5a0976a7e69823e75ace7c1b0703fa032075301e
Harry <harryw@posteo.uk>
2023-09-22 17:20:35 +0100

    updates businessListItem

--- front-end/snacktrack/pages/businesses/components/businessListItem.jsx
+++ front-end/snacktrack/pages/businesses/components/businessListItem.jsx
@@ -2,7 +2,29 @@ export default function BusinessListItem({ business }) {
   return (
     <>
       <ul>
-        <li key={business._id}>{business.business_name}`</li>
+        <li key={business._id}>
+          {business.business_name}
+          <div className="card">
+            <div className="card-image"></div>
+            <div className="card-content">
+              <div className="media">
+                <div className="media-left"></div>
+                <div className="media-content">
+                  <p className="title is-4">{business.business_name}</p>
+                  <p></p>
+                  <p className="subtitle is-6">
+                    Rating: {business.total_rating}({business.no_of_ratings})
+                  </p>
+                  <p className="subtitle is-6">
+                    {business.is_active === true ? "Active now" : "Inactive"}
+                  </p>
+                </div>
+              </div>
+              <div className="content"></div>
+            </div>
+          </div>
+          <br></br>
+        </li>
       </ul>
     </>
   );
