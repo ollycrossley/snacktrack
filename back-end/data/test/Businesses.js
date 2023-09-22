a94dc75e2e9341ccf22420f16f34697321b49467
RichRavenM <richard.s.morris@btinternet.com>
2023-09-22 17:46:46 +0100

    amends testdata

--- back-end/data/test/Businesses.js
+++ back-end/data/test/Businesses.js
@@ -9,7 +9,8 @@ module.exports = [
     business_name: "Twiyo",
     business_bio:
       "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.",
-    logo_url: "http://dummyimage.com/212x114.png/ff4444/ffffff",
+    logo_url:
+      "https://www.foodandwine.com/thmb/pwFie7NRkq4SXMDJU6QKnUKlaoI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Ultimate-Veggie-Burgers-FT-Recipe-0821-5d7532c53a924a7298d2175cf1d4219f.jpg",
     location: {
       latitude: 53.478948272998124,
       longitude: -2.2413653711839294,
@@ -24,6 +25,8 @@ module.exports = [
       saturday: ["11:00", "21:00"],
       sunday: ["09:00", "20:00"],
     },
+    total_rating: 18,
+    no_of_ratings: 6,
   },
   {
     username: "bvize1",
@@ -35,7 +38,8 @@ module.exports = [
     business_name: "Innojam",
     business_bio:
       "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.",
-    logo_url: "http://dummyimage.com/249x205.png/cc0000/ffffff",
+    logo_url:
+      "https://img.taste.com.au/w_-0kcUJ/taste/2016/11/aussie-style-beef-and-salad-tacos-86525-1.jpeg",
     location: {
       latitude: 5.976474,
       longitude: 116.115777,
@@ -49,6 +53,8 @@ module.exports = [
       saturday: ["11:00", "21:00"],
       sunday: ["09:00", "20:00"],
     },
+    total_rating: 6,
+    no_of_ratings: 2,
   },
   {
     username: "bowens2",
@@ -61,7 +67,8 @@ module.exports = [
     is_active: true,
     business_bio:
       "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.\n\nMorbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.\n\nFusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
-    logo_url: "http://dummyimage.com/163x104.png/5fa2dd/ffffff",
+    logo_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Strawberry_ice_cream_cone_%285076899310%29.jpg/640px-Strawberry_ice_cream_cone_%285076899310%29.jpg",
     menu_url:
       "https://t3.ftcdn.net/jpg/01/75/06/34/360_F_175063465_nPAUPd3x4uoqbmKyGqDLRDsIvMejnraQ.jpg",
     location: {
@@ -88,7 +95,8 @@ module.exports = [
     business_name: "Quimm",
     business_bio:
       "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.",
-    logo_url: "http://dummyimage.com/175x222.png/5fa2dd/ffffff",
+    logo_url:
+      "https://media-cdn.tripadvisor.com/media/photo-s/0e/20/59/91/photo0jpg.jpg",
     menu_url:
       "https://t3.ftcdn.net/jpg/01/75/06/34/360_F_175063465_nPAUPd3x4uoqbmKyGqDLRDsIvMejnraQ.jpg",
     location: {
@@ -104,11 +112,13 @@ module.exports = [
       saturday: ["11:00", "21:00"],
       sunday: ["09:00", "20:00"],
     },
+    total_rating: 15,
+    no_of_ratings: 4,
   },
   {
     username: "pneames4",
     email: "plillford4@a8.net",
-    category: "Balloon",
+    category: "Clothes",
     password: "wY9|>RGk_/8",
     avatar_url: "http://dummyimage.com/207x140.png/dddddd/000000",
     owner_name: "Pandora",
@@ -116,7 +126,7 @@ module.exports = [
     is_active: true,
     business_bio:
       "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.\n\nFusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.\n\nSed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.",
-    logo_url: "http://dummyimage.com/118x191.png/ff4444/ffffff",
+    logo_url: "https://m.media-amazon.com/images/I/41GpYNxJGxL._AC_SY350_.jpg",
     location: {
       latitude: 53.483864805152116,
       longitude: -2.2009522870568796,
@@ -134,14 +144,15 @@ module.exports = [
   {
     username: "clissandre5",
     email: "cstiven5@shop-pro.jp",
-    category: "Balloon",
+    category: "Clothes",
     password: 'kN3|DC.w+x=2"N',
     avatar_url: "http://dummyimage.com/212x104.png/cc0000/ffffff",
     owner_name: "Clemmie",
     business_name: "Blogtags",
     business_bio:
       "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.",
-    logo_url: "http://dummyimage.com/109x189.png/cc0000/ffffff",
+    logo_url:
+      "https://q8y9x5z7.rocketcdn.me/wp-content/uploads/2020/05/web-classic-select-brisa-weave-classic-fedora-pachacuti-mens-sun-panama-hat-5-category-v2-1000x750.jpg",
     menu_url:
       "https://t3.ftcdn.net/jpg/01/75/06/34/360_F_175063465_nPAUPd3x4uoqbmKyGqDLRDsIvMejnraQ.jpg",
     location: {
@@ -164,12 +175,13 @@ module.exports = [
     password: "zN2,|{9tbyc",
     avatar_url: "http://dummyimage.com/164x220.png/cc0000/ffffff",
     owner_name: "Karol",
-    category: "Balloon",
+    category: "Clothes",
     is_active: true,
     business_name: "Youbridge",
     business_bio:
       "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.",
-    logo_url: "http://dummyimage.com/239x193.png/cc0000/ffffff",
+    logo_url:
+      "https://www.sweatershop.com/media/catalog/product/cache/d2d224c54d520941388cc48be6fe21e1/6/_/6_1_2.jpg",
     location: {
       latitude: 53.464679060673674,
       longitude: -2.2905663701724035,
@@ -183,10 +195,12 @@ module.exports = [
       saturday: ["11:00", "21:00"],
       sunday: ["09:00", "20:00"],
     },
+    total_rating: 32,
+    no_of_ratings: 10,
   },
   {
     username: "pklimowicz7",
-    category: "Umbrellas",
+    category: "Souvenir",
     email: "pmcavaddy7@usgs.gov",
     password: 'cR6"Kt/#',
     avatar_url: "http://dummyimage.com/247x171.png/5fa2dd/ffffff",
@@ -194,7 +208,7 @@ module.exports = [
     business_name: "Jabbertype",
     business_bio:
       "In congue. Etiam justo. Etiam pretium iaculis justo.\n\nIn hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
-    logo_url: "http://dummyimage.com/161x118.png/cc0000/ffffff",
+    logo_url: "https://static.independent.co.uk/2022/03/16/13/newFile-5.jpg",
     menu_url: "https://imenupro.com/img/menu-template-steakhouse.png",
     location: {
       latitude: -7.1516288,
@@ -213,7 +227,7 @@ module.exports = [
   {
     username: "ealfonsetti8",
     email: "ealexander8@issuu.com",
-    category: "Umbrellas",
+    category: "Souvenir",
     password: "tA1<Bz*A#Pc*/6F",
     avatar_url: "http://dummyimage.com/203x218.png/5fa2dd/ffffff",
     owner_name: "Edy",
@@ -221,7 +235,8 @@ module.exports = [
     business_name: "Wikivu",
     business_bio:
       "Sed ante. Vivamus tortor. Duis mattis egestas metus.\n\nAenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
-    logo_url: "http://dummyimage.com/138x241.png/ff4444/ffffff",
+    logo_url:
+      "https://vickyflipfloptravels.com/wp-content/uploads/2023/03/unique-souvenir-ideas.jpg.webp",
     location: {
       latitude: 53.477716513428426,
       longitude: -2.2540067761062765,
@@ -237,11 +252,13 @@ module.exports = [
       saturday: ["11:00", "21:00"],
       sunday: ["09:00", "20:00"],
     },
+    total_rating: 13,
+    no_of_ratings: 3,
   },
   {
     username: "tweiss9",
     email: "tponcet9@shutterfly.com",
-    category: "Umbrellas",
+    category: "Food",
     is_active: true,
     password: 'jA7+"Iw9',
     avatar_url: "http://dummyimage.com/205x129.png/cc0000/ffffff",
@@ -249,7 +266,8 @@ module.exports = [
     business_name: "Photolist",
     business_bio:
       "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.",
-    logo_url: "http://dummyimage.com/104x238.png/cc0000/ffffff",
+    logo_url:
+      "https://cdn-prod.medicalnewstoday.com/content/images/articles/325/325253/assortment-of-fruits.jpg",
     menu_url:
       "https://marketplace.canva.com/EAFKfB87pN0/1/0/1131w/canva-brown-and-black-illustration-fast-food-menu-y8NpubROdFc.jpg",
     location: {
