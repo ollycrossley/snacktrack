9021895a0f0aeb425ef2b48a3509752c821aa39f
Oliver Crossley <ollythecrossley@outlook.com>
2023-09-22 14:44:31 +0100

    converts to new map api

--- front-end/snacktrack/pages/map/components/simplemap.jsx
+++ front-end/snacktrack/pages/map/components/simplemap.jsx
@@ -1,64 +1,65 @@
-import GoogleMapReact from "google-map-react";
+import {GoogleMap, useJsApiLoader, MarkerF} from '@react-google-maps/api';
+
 import React, {useEffect, useState} from "react";
 import {getBusinesses} from "@/api";
 
 export default function SimpleMap() {
-  const AnyReactComponent = ({ text }) => <div>{text}</div>;
+    const [businesses, setBusinesses] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
+    // const [map, setMap] = useState(null)
 
-  const [businesses, setBusinesses] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+    })
 
-  const defaultProps = {
-    center: {
-      lat: 51.904512,
-      lng: -2.1037056,
-    },
-    zoom: 1,
-  };
+    const defaultProps = {
+        center: {
+            lat: 53.47225548949146,
+            lng: -2.238625720021971,
+        },
+        zoom: 13,
+    };
 
-  // zoom: 16,
 
-  useEffect(() => {
-    setIsLoading(true)
-    getBusinesses()
-        .then(businesses => {
-          setBusinesses(businesses)
-          setIsLoading(false)
-        })
+    // zoom: 16,
 
-  }, []);
+    useEffect(() => {
+        setIsLoading(true)
+        getBusinesses()
+            .then(businesses => {
+                setBusinesses(businesses)
+                setIsLoading(false)
+            })
 
-  if (isLoading) {
-    return <h1>Loading...</h1>
-  }
+    }, []);
 
-  return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: "100vh", width: "100%" }}>
-        <GoogleMapReact
-            bootstrapURLKeys={{ key: "" }}
-            defaultCenter={defaultProps.center}
-            defaultZoom={defaultProps.zoom}
-        >
-          {businesses.map(business => {
-            if (business.is_active === true) {
-              return (
-                  <AnyReactComponent
-                      lat={business.location.latitude}
-                      lng={business.location.longitude}
-                      text={
-                        <span className={"icon is-size-4"}>
-                    <i
-                        className="fa-solid fa-location-dot"
-                        style={{ color: "#ff0000" }}
-                    ></i>
-                  </span>
-                      }
-                  />
-              );
-            }
-          })}
-        </GoogleMapReact>
-      </div>
-  );
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
+
+    return isLoaded ? (
+        // Important! Always set the container height explicitly
+            <GoogleMap
+                mapContainerStyle={{height: "100vh", width: "100%"}}
+                center={defaultProps.center}
+                zoom={defaultProps.zoom}
+                onLoad={map => {
+                    const bounds = new window.google.maps.LatLngBounds(defaultProps.center);
+                    // map.fitBounds(bounds);
+                }}
+            >
+                {businesses.map(business => {
+                    if (business.is_active === true) {
+                        return (
+                            <MarkerF
+                             position={{lat: business.location.latitude, lng: business.location.longitude}}
+                             icon={{url: "https://i.ibb.co/cQPqCjF/custom-snacktrack-icon.png", scaledSize: new google.maps.Size(38, 38)}}
+                            />
+
+                        )
+                    }
+                })}
+                <></>
+            </GoogleMap>
+    ) : <></>
 }
