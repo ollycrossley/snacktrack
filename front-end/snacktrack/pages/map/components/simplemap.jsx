d1c045ee0f38a17288241552a67771b1010508a3
danny-817 <d_halsall@hotmail.com>
2023-09-22 15:37:54 +0100

    adds keys to the listed map pins

--- front-end/snacktrack/pages/map/components/simplemap.jsx
+++ front-end/snacktrack/pages/map/components/simplemap.jsx
@@ -1,12 +1,12 @@
 import GoogleMapReact from "google-map-react";
-import React, {useEffect, useState} from "react";
-import {getBusinesses} from "@/api";
+import React, { useEffect, useState } from "react";
+import { getBusinesses } from "@/api";
 
 export default function SimpleMap() {
   const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
   const [businesses, setBusinesses] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   const defaultProps = {
     center: {
@@ -19,46 +19,45 @@ export default function SimpleMap() {
   // zoom: 16,
 
   useEffect(() => {
-    setIsLoading(true)
-    getBusinesses()
-        .then(businesses => {
-          setBusinesses(businesses)
-          setIsLoading(false)
-        })
-
+    setIsLoading(true);
+    getBusinesses().then((businesses) => {
+      setBusinesses(businesses);
+      setIsLoading(false);
+    });
   }, []);
 
   if (isLoading) {
-    return <h1>Loading...</h1>
+    return <h1>Loading...</h1>;
   }
 
   return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: "100vh", width: "100%" }}>
-        <GoogleMapReact
-            bootstrapURLKeys={{ key: "" }}
-            defaultCenter={defaultProps.center}
-            defaultZoom={defaultProps.zoom}
-        >
-          {businesses.map(business => {
-            if (business.is_active === true) {
-              return (
-                  <AnyReactComponent
-                      lat={business.location.latitude}
-                      lng={business.location.longitude}
-                      text={
-                        <span className={"icon is-size-4"}>
+    // Important! Always set the container height explicitly
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        {businesses.map((business) => {
+          if (business.is_active === true) {
+            return (
+              <AnyReactComponent
+                key={`${business._id}`}
+                lat={business.location.latitude}
+                lng={business.location.longitude}
+                text={
+                  <span className={"icon is-size-4"}>
                     <i
-                        className="fa-solid fa-location-dot"
-                        style={{ color: "#ff0000" }}
+                      className="fa-solid fa-location-dot"
+                      style={{ color: "#ff0000" }}
                     ></i>
                   </span>
-                      }
-                  />
-              );
-            }
-          })}
-        </GoogleMapReact>
-      </div>
+                }
+              />
+            );
+          }
+        })}
+      </GoogleMapReact>
+    </div>
   );
 }
