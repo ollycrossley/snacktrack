a929d798c2cf1a565c193bf908b591317d2a7139
RichRavenM <richard.s.morris@btinternet.com>
2023-09-22 09:33:15 +0100

    separates production seed

--- back-end/seed-production.js
+++ back-end/seed-production.js
@@ -1,22 +1,24 @@
-
 const mongoose = require("mongoose");
+const { seed } = require("./seed");
 
-const app = require("./app");
+const customerData = require("./data/test/Customers");
+const businessData = require("./data/test/Businesses");
+const reviewData = require("./data/test/Reviews");
 
 const ENV = "production";
 
 const pathToCorrectFile = `${__dirname}/.env.${ENV}`;
 require("dotenv").config({ path: pathToCorrectFile });
-const connectDB = async () => {
+
+const seedDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    app.listen(9090, () => console.log("Server started on port 9090"));
+    await seed(customerData, businessData, reviewData);
+    await mongoose.connection.close();
   } catch (error) {
     console.error("connection failed" + error);
     process.exit(1);
   }
 };
 
-connectDB();
-
-module.exports = connectDB;
+seedDB();
