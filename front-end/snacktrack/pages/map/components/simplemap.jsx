de0cf688d9281704109d2b61c16304297d19fcdd
Oliver Crossley <ollythecrossley@outlook.com>
2023-09-22 17:15:05 +0100

    completes map popups with link

--- front-end/snacktrack/pages/map/components/simplemap.jsx
+++ front-end/snacktrack/pages/map/components/simplemap.jsx
@@ -1,16 +1,28 @@
-import {GoogleMap, useJsApiLoader, MarkerF} from '@react-google-maps/api';
+import {GoogleMap, useJsApiLoader, MarkerF, Marker, InfoWindow, InfoWindowF} from '@react-google-maps/api';
 
 import React, {useEffect, useState} from "react";
 import {getBusinesses} from "@/api";
+import Link from "next/link";
 
 export default function SimpleMap() {
     const [businesses, setBusinesses] = useState([]);
     const [isLoading, setIsLoading] = useState(false)
+    const [isModalActive, setIsModalActive] = useState(false)
+    const [modal, setModal] = useState(<></>)
     // const [map, setMap] = useState(null)
 
-    const { isLoaded } = useJsApiLoader({
+    const {isLoaded} = useJsApiLoader({
         id: 'google-map-script',
     })
+
+    const [activeMarker, setActiveMarker] = useState(null);
+
+    const handleActiveMarker = (marker) => {
+        /*if (marker === activeMarker) {
+            return;
+        }*/
+        setActiveMarker(marker);
+    };
 
     const defaultProps = {
         center: {
@@ -19,7 +31,6 @@ export default function SimpleMap() {
         },
         zoom: 13,
     };
-
 
     // zoom: 16,
 
@@ -39,10 +50,12 @@ export default function SimpleMap() {
 
     return isLoaded ? (
         // Important! Always set the container height explicitly
+        <div>
             <GoogleMap
                 mapContainerStyle={{height: "100vh", width: "100%"}}
                 center={defaultProps.center}
                 zoom={defaultProps.zoom}
+                onClick={() => setActiveMarker(null)}
                 onLoad={map => {
                     const bounds = new window.google.maps.LatLngBounds(defaultProps.center);
                     // map.fitBounds(bounds);
@@ -52,14 +65,22 @@ export default function SimpleMap() {
                     if (business.is_active === true) {
                         return (
                             <MarkerF
-                             position={{lat: business.location.latitude, lng: business.location.longitude}}
-                             icon={{url: "https://i.ibb.co/cQPqCjF/custom-snacktrack-icon.png", scaledSize: new google.maps.Size(38, 38)}}
-                            />
-
+                                position={{lat: business.location.latitude, lng: business.location.longitude}}
+                                icon={{
+                                    url: "https://i.ibb.co/cQPqCjF/custom-snacktrack-icon.png",
+                                    scaledSize: new google.maps.Size(38, 38)
+                                }}
+                                onClick={() => handleActiveMarker(business.business_name)}>
+                                {activeMarker === business.business_name ? (
+                                    <InfoWindowF onCloseClick={() => setActiveMarker(null)}>
+                                        <Link href={`/businesses/${business._id}`}><div>{business.business_name}</div></Link>
+                                    </InfoWindowF>
+                                ) : null}
+                            </MarkerF>
                         )
                     }
                 })}
-                <></>
             </GoogleMap>
+        </div>
     ) : <></>
 }
