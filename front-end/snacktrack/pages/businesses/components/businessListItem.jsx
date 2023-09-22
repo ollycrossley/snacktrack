863057980342cbd246dde5d71908c03cf34c6172
Harry <harryw@posteo.uk>
2023-09-22 13:39:55 +0100

    adds some business page styling

--- front-end/snacktrack/pages/businesses/components/businessListItem.jsx
+++ front-end/snacktrack/pages/businesses/components/businessListItem.jsx
@@ -1,10 +1,9 @@
 export default function BusinessListItem({ business }) {
   return (
     <>
-      <tr key={business._id}>
-        <th>{business.business_name}</th>
-        <th>{business.total_rating}</th>
-      </tr>
+      <ul>
+        <li key={business._id}>{business.business_name}`</li>
+      </ul>
     </>
   );
 }
--- front-end/snacktrack/pages/businesses/index.jsx
+++ front-end/snacktrack/pages/businesses/index.jsx
@@ -4,10 +4,11 @@ import { useEffect, useState } from "react";
 import { getBusinesses } from "@/api";
 import React from "react";
 import BusinessListItem from "./components/businessListItem";
+// import "../../../snacktrack/styles/globals.scss";
 
 export default function allBusinesses() {
-  const [businesses, setBusinesses] = useState();
-  const [isLoading, setIsLoading] = useState(true);
+  const [businesses, setBusinesses] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     setIsLoading(true);
     getBusinesses().then((r) => {
@@ -20,21 +21,49 @@ export default function allBusinesses() {
   }
   return (
     <>
+      {console.log(businesses)}
       <NavBar />
-      <br></br>
-      <h1>Businesses List{console.log(businesses)}</h1>
-      <br></br>
-      <table>
-        <thead>
-          <tr>Business Name</tr>
-          <tr>Rating</tr>
-        </thead>
-        <tbody>
-          {businesses.map((business) => (
-            <BusinessListItem business={business} />
-          ))}
-        </tbody>
-      </table>
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <ul>
+                {businesses.map((business) => {
+                  return (
+                    <li key={business.business_name}>
+                      <div className="card">
+                        <div className="card-image"></div>
+                        <div className="card-content">
+                          <div className="media">
+                            <div className="media-left"></div>
+                            <div className="media-content">
+                              <p className="title is-4">
+                                {business.business_name}
+                              </p>
+                              <p></p>
+                              <p className="subtitle is-6">
+                                Rating: {business.total_rating}(
+                                {business.no_of_ratings})
+                              </p>
+                              <p className="subtitle is-6">
+                                {business.is_active === true
+                                  ? "Active now"
+                                  : "Inactive"}
+                              </p>
+                            </div>
+                          </div>
+                          <div className="content"></div>
+                        </div>
+                      </div>
+                      <br></br>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
       <ul></ul>
     </>
   );
