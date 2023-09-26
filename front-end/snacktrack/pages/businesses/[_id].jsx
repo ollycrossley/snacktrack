623c8abc3b3cf87719436d7fe23b65d232af478e
Oliver Crossley <ollythecrossley@outlook.com>
2023-09-26 17:37:14 +0100

    starts styling on single business page

--- front-end/snacktrack/pages/businesses/[_id].jsx
+++ front-end/snacktrack/pages/businesses/[_id].jsx
@@ -1,10 +1,12 @@
 import NavBar from "@/pages/navbar";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useContext, useState } from "react";
 import { UserContext } from "@/contexts/user_context";
 import { getSingleBusiness, getReviews } from "@/api";
-import AddReview from "@/components/addReview";
-import DeleteReview from "@/components/deleteReview";
+import Reviews from "../../components/businessreviews";
+import AddReview from "../../components/addReview";
+import DeleteReview from "../../components/deleteReview";
+import Head from "next/head";
 
 export async function getServerSideProps(context) {
   const { _id } = context.query;
@@ -57,106 +59,125 @@ export default function singleBusiness({ _id }) {
 
   return (
     <>
+        <Head>
+            <script
+                src="https://kit.fontawesome.com/a1a2d1f110.js"
+                crossOrigin="anonymous"
+            />
+        </Head>
       <NavBar />
-      <br></br>
-      <h1>{business.business_name}</h1>
-      <br></br>
-      <h1>
-        {business.is_active ? "Open Now!" : "This business is currently closed"}
-      </h1>
-      <br></br>
-      <h1>Business Type: {business.category}</h1>
-      <br></br>
-      <ul>
-        <li key="Monday">
-          <h1 className="day">Monday:</h1>
-          <h1 className="hours">
-            {monOpeningHours[0]} - {monOpeningHours[1]}
-          </h1>
-        </li>
-        <li key="Tuesday">
-          <h1 className="day">Tuesday:</h1>
-          <h1 className="hours">
-            {tueOpeningHours[0]} - {tueOpeningHours[1]}
-          </h1>
-        </li>
-        <li key="Wednesday">
-          <h1 className="day">Wednesday:</h1>
-          <h1 className="hours">
-            {wedOpeningHours[0]} - {wedOpeningHours[1]}
-          </h1>
-        </li>
-        <li key="Thursday">
-          <h1 className="day">Thursday:</h1>
-          <h1 className="hours">
-            {thuOpeningHours[0]} - {thuOpeningHours[1]}
-          </h1>
-        </li>
-        <li key="Friday">
-          <h1 className="day">Friday:</h1>
-          <h1 className="hours">
-            {friOpeningHours[0]} - {monOpeningHours[1]}
-          </h1>
-        </li>
-        <li key="Saturday">
-          <h1 className="day">Saturday:</h1>
-          <h1 className="hours">
-            {satOpeningHours[0]} - {satOpeningHours[1]}
-          </h1>
-        </li>
-        <li key="Sunday">
-          <h1 className="day">Sunday:</h1>
-          <h1 className="hours">
-            {sunOpeningHours[0]} - {sunOpeningHours[1]}
-          </h1>
-        </li>
-      </ul>
-      <br></br>
-      {activeUser._id && !activeUser.is_active ? (
-        <AddReview
-          business_id={business._id}
-          customer_id={activeUser._id}
-          customer_avatar_url={activeUser.avatar_url}
-          customer_username={activeUser.username}
-          reviewsArray={reviewsArray}
-          setReviewsArray={setReviewsArray}
-          setTotalRating={setTotalRating}
-          setNumberOfRatings={setNumberOfRatings}
-        />
-      ) : null}
-      <h1>
-        Average Review:
-        {numberOfRatings === 0
-          ? 0
-          : Number(totalRating / numberOfRatings).toFixed(1)}
-      </h1>
-      <img src={business.menu_url} alt="the businesses menu" />
-      <div>
-        <h1>Reviews:</h1>
-        <ul>
-          {reviewsArray.map((review) => {
-            return (
-              <li key={`${review._id}`}>
-                <h1>Username: {review.customerUsername}</h1>
-                <h1>{review.rating}/5</h1>
-                <img src={review.customerAvatarUrl}></img>
-                <p>{review.body}</p>
-                {activeUser._id === review.customer_id ? (
-                  <DeleteReview
-                    review_id={review._id}
+        <div className={"container"}>
+            <div className={"box"}>
+                <p className={"title is-size-1"}>{business.business_name}</p>
+                <p className={"subtitle"}>{business.category}</p>
+                <span className={"icon-text mb-2 mr-4"}>
+                    <span className={"icon pb-2"}><i className="fa-solid fa-star" style={{color: "#f1cb29"}}></i></span>
+                    <span>{numberOfRatings === 0
+                        ? 0
+                        : Number(totalRating / numberOfRatings).toFixed(1)}</span>
+                </span>
+                <span className={"icon-text mb-2"}>
+                    <span className={"icon pb-1"}><i className="fa-solid fa-circle-dot" style={{color: "#01b20d"}}></i></span>
+                    <span>{business.is_active ? "Open" : "This business is currently closed"}</span>
+                </span>
+
+            </div>
+            <br></br>
+
+            <div className={"columns"}>
+                <div className={"column is-half"}>
+                    <div className={"box"}>
+                        <h1 className={"title"}>Schedule</h1>
+                        <table className={"table is-fullwidth"}>
+                            <thead>
+                                <tr>
+                                    <th>Day</th>
+                                    <th>Open</th>
+                                    <th>Closed</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>Monday</th>
+                                    <td>{monOpeningHours[0]}</td>
+                                    <td>{monOpeningHours[1]}</td>
+                                </tr>
+                                <tr>
+                                    <th>Tuesday</th>
+                                    <td>{tueOpeningHours[0]}</td>
+                                    <td>{tueOpeningHours[1]}</td>
+                                </tr>
+                                <tr>
+                                    <th>Wednesday</th>
+                                    <td>{wedOpeningHours[0]}</td>
+                                    <td>{wedOpeningHours[1]}</td>
+                                </tr>
+                                <tr>
+                                    <th>Thursday</th>
+                                    <td>{thuOpeningHours[0]}</td>
+                                    <td>{thuOpeningHours[1]}</td>
+                                </tr>
+                                <tr>
+                                    <th>Friday</th>
+                                    <td>{friOpeningHours[0]}</td>
+                                    <td>{friOpeningHours[1]}</td>
+                                </tr>
+                                <tr>
+                                    <th>Saturday</th>
+                                    <td>{satOpeningHours[0]}</td>
+                                    <td>{satOpeningHours[1]}</td>
+                                </tr>
+                                <tr>
+                                    <th>Sunday</th>
+                                    <td>{sunOpeningHours[0]}</td>
+                                    <td>{sunOpeningHours[1]}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <br></br>
+            {activeUser._id && !activeUser.is_active ? (
+                <AddReview
                     business_id={business._id}
-                    reviewRating={review.rating}
+                    customer_id={activeUser._id}
+                    customer_avatar_url={activeUser.avatar_url}
+                    customer_username={activeUser.username}
+                    reviewsArray={reviewsArray}
+                    setReviewsArray={setReviewsArray}
                     setTotalRating={setTotalRating}
                     setNumberOfRatings={setNumberOfRatings}
-                    setReviewsArray={setReviewsArray}
-                    reviewsArray={reviewsArray}
-                  />
-                ) : null}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+                />
+            ) : null}
+            <img src={business.menu_url} alt="the businesses menu"/>
+            <div>
+                <h1>Reviews:</h1>
+                <ul>
+                    {reviewsArray.map((review) => {
+                        return (
+                            <li key={`${review._id}`}>
+                                <h1>Username: {review.customerUsername}</h1>
+                                <h1>{review.rating}/5</h1>
+                                <img src={review.customerAvatarUrl}></img>
+                                <p>{review.body}</p>
+                                {activeUser._id === review.customer_id ? (
+                                    <DeleteReview
+                                        review_id={review._id}
+                                        business_id={business._id}
+                                        reviewRating={review.rating}
+                                        setTotalRating={setTotalRating}
+                                        setNumberOfRatings={setNumberOfRatings}
+                                        setReviewsArray={setReviewsArray}
+                                        reviewsArray={reviewsArray}
+                                    />
+                                ) : null}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
     </>
   );
 }
