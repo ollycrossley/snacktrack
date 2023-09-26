df2a58a97ffa018f1303c67cba5130094a7316f6
Oliver Crossley <ollythecrossley@outlook.com>
2023-09-26 11:17:05 +0100

    update map to have buttons for driver and customer

--- front-end/snacktrack/pages/map/index.jsx
+++ front-end/snacktrack/pages/map/index.jsx
@@ -4,63 +4,92 @@ import Head from "next/head";
 import Script from "next/script";
 import {useContext, useEffect, useState} from "react";
 import {UserContext} from "@/contexts/user_context";
+import {patchBusiness} from "@/api";
 
 export default function Map() {
-  const [myCrd, setMyCrd] = useState({});
-    const { activeUser, setActiveUser } = useContext(UserContext);
+    const [myCrd, setMyCrd] = useState({});
+    const {activeUser, setActiveUser} = useContext(UserContext);
 
-  function success(pos) {
-    const crd = pos.coords;
-    setMyCrd(crd);
-    // console.log("Your current position is:");
-    // console.log(`Latitude : ${crd.latitude}`);
-    // console.log(`Longitude: ${crd.longitude}`);
-    // console.log(`More or less ${crd.accuracy} meters.`);
-  }
-  const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
-
-  function errors(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.permissions
-        .query({ name: "geolocation" })
-        .then(function (result) {
-          console.log(result);
-          if (result.state === "granted") {
-            navigator.geolocation.getCurrentPosition(success, errors, options);
-          } else if (result.state === "prompt") {
-            navigator.geolocation.getCurrentPosition(success, errors, options);
-            console.log("We are in prompt mode!");
-          } else if (result.state === "denied") {
-            // do other things
-          }
-        });
-    } else {
-      console.log("Geolocation is not supported by this browser");
+    function success(pos) {
+        const crd = pos.coords;
+        setMyCrd(crd);
+        // console.log("Your current position is:");
+        // console.log(`Latitude : ${crd.latitude}`);
+        // console.log(`Longitude: ${crd.longitude}`);
+        // console.log(`More or less ${crd.accuracy} meters.`);
     }
-    console.log(navigator);
-  }, []);
 
-  return (
-    <>
-      <Head>
-        <script
-          src="https://kit.fontawesome.com/a1a2d1f110.js"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
-      <NavBar />
-      <h1 className={"title has-text-centered"}>MAP</h1>
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    };
 
-        {activeUser ? <div className={"container"} style={{marginBottom: 25}}>
-            <SimpleMap userLat={myCrd.latitude} userLong={myCrd.longitude}/>
-        </div> : <div><br/><br/><br/><h1 className={"title has-text-centered"}>You need to be logged in to use the map!</h1></div>}
-    </>
-  );
+    function errors(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    function handleActive () {
+        activeUser.is_active = !activeUser.is_active
+        patchBusiness({is_active: activeUser.is_active}, activeUser._id).then(business => setActiveUser(business))
+    }
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.permissions
+                .query({name: "geolocation"})
+                .then(function (result) {
+                    console.log(result);
+                    if (result.state === "granted") {
+                        navigator.geolocation.getCurrentPosition(success, errors, options);
+                    } else if (result.state === "prompt") {
+                        navigator.geolocation.getCurrentPosition(success, errors, options);
+                        console.log("We are in prompt mode!");
+                    } else if (result.state === "denied") {
+                        // do other things
+                    }
+                });
+        } else {
+            console.log("Geolocation is not supported by this browser");
+        }
+        console.log(navigator);
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <script
+                    src="https://kit.fontawesome.com/a1a2d1f110.js"
+                    crossOrigin="anonymous"
+                ></script>
+            </Head>
+            <NavBar/>
+
+            <h1 className={"title has-text-centered"}>MAP</h1>
+
+            {activeUser ?
+                <div className={"container has-text-centered"} style={{marginBottom: 25}}>
+                   <div className={"columns is-centered"}>
+
+                       {"is_active" in activeUser ? <div className={"column is-one-fifth"}>
+                            <button
+                                className={`button ${activeUser.is_active ? "is-success is-light" : "is-danger is-light"}`} onClick={handleActive}>
+                                {activeUser.is_active ? "Currently Open" : "Press to open"}
+                            </button>
+                        </div>: null}
+
+                        <div className={"column is-one-fifth"}>
+                            <button className={"button is-link is-light"} onClick={() => window.location.reload()}>Refresh Location</button>
+                        </div>
+                        
+                        
+                        <br/><br/><br/>
+                    </div>
+                    <SimpleMap userLat={myCrd.latitude} userLong={myCrd.longitude}/>
+                </div>
+                :
+                <div><br/><br/><br/><h1 className={"title has-text-centered"}>You need to be logged in to use the
+                    map!</h1></div>}
+        </>
+    );
 }
