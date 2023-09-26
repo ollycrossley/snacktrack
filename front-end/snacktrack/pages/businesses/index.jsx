4ab8423a8e6ba39501b5b6ca54efc8fb4c5d4f38
LeopoldHarvey <LeopoldHarvey42@gmail.com>
2023-09-26 12:05:55 +0100

    added the code to link the businesses to the review pages

--- front-end/snacktrack/pages/businesses/index.jsx
+++ front-end/snacktrack/pages/businesses/index.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { getBusinesses } from "@/api";
 import React from "react";
 import BusinessListItem from "./components/businessListItem";
+import Link from "next/link";
 // import "../../../snacktrack/styles/globals.scss";
 
 export default function allBusinesses() {
@@ -31,31 +32,33 @@ export default function allBusinesses() {
                 {businesses.map((business) => {
                   return (
                     <li key={business.business_name}>
-                      <div className="card">
-                        <div className="card-image"></div>
-                        <div className="card-content">
-                          <div className="media">
-                            <div className="media-left"></div>
-                            <div className="media-content">
-                              <p className="title is-4">
-                                {business.business_name}
-                              </p>
-                              <p></p>
-                              <p className="subtitle is-6">
-                                Rating: {business.total_rating}(
-                                {business.no_of_ratings})
-                              </p>
-                              <p className="subtitle is-6">
-                                {business.is_active === true
-                                  ? "Active now"
-                                  : "Inactive"}
-                              </p>
+                      <Link href={`/businesses/${business._id}`}>
+                        <div className="card">
+                          <div className="card-image"></div>
+                          <div className="card-content">
+                            <div className="media">
+                              <div className="media-left"></div>
+                              <div className="media-content">
+                                <p className="title is-4">
+                                  {business.business_name}
+                                </p>
+                                <p></p>
+                                <p className="subtitle is-6">
+                                  Rating: {business.total_rating}(
+                                  {business.no_of_ratings})
+                                </p>
+                                <p className="subtitle is-6">
+                                  {business.is_active === true
+                                    ? "Active now"
+                                    : "Inactive"}
+                                </p>
+                              </div>
                             </div>
+                            <div className="content"></div>
                           </div>
-                          <div className="content"></div>
                         </div>
-                      </div>
-                      <br></br>
+                        <br></br>
+                      </Link>
                     </li>
                   );
                 })}
