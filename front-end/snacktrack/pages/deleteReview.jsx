37a95a1fd6a8519a5b3d7b826e754f4b5d4e3e03
RichRavenM <richard.s.morris@btinternet.com>
2023-09-26 11:34:20 +0100

    adds delete review feature

---
+++ front-end/snacktrack/pages/deleteReview.jsx
@@ -0,0 +1,59 @@
+import { deleteReview, patchBusinessRating } from "@/api";
+
+export default function DeleteReview({
+  review_id,
+  business_id,
+  reviewRating,
+  setTotalRating,
+  setNumberOfRatings,
+  setReviewsArray,
+  reviewsArray,
+}) {
+  const handleClick = (e) => {
+    confirm("Are you sure you wan to delete this?");
+    e.preventDefault();
+    const ratingToTake = reviewRating * -1;
+    const reviewToTake = reviewsArray[0];
+    const copyReviews = [...reviewsArray];
+    setReviewsArray((currReviews) => {
+      const output = [...reviewsArray];
+      output.shift();
+      return output;
+    });
+    setTotalRating((currRating) => {
+      return currRating - reviewRating;
+    });
+    setNumberOfRatings((currNum) => {
+      return currNum - 1;
+    });
+    Promise.all([
+      deleteReview(review_id),
+      patchBusinessRating(business_id, {
+        rating: ratingToTake,
+        no_of_ratings: -1,
+      }),
+    ])
+      .then(() => {
+        alert("Review deleted");
+      })
+      .catch((e) => {
+        setReviewsArray(copyReviews);
+        setTotalRating((currRating) => {
+          return currRating + reviewRating;
+        });
+        setNumberOfRatings((currNum) => {
+          return currNum + 1;
+        });
+        alert("Error. Please refresh and try again");
+      });
+  };
+  return (
+    <button
+      onClick={(e) => {
+        handleClick(e);
+      }}
+    >
+      Delete Review
+    </button>
+  );
+}
