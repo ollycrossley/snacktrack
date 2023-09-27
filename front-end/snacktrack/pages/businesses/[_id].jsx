f244bfffb14b23d7cf5db6419f67e0090d0c16aa
Oliver Crossley <ollythecrossley@outlook.com>
2023-09-27 10:34:38 +0100

    adds modal to image clickable

--- front-end/snacktrack/pages/businesses/[_id].jsx
+++ front-end/snacktrack/pages/businesses/[_id].jsx
@@ -37,6 +37,8 @@ export default function singleBusiness({ _id }) {
   const [totalRating, setTotalRating] = useState("");
   const [reviewsArray, setReviewsArray] = useState([{}]);
 
+  const [isModalActive, setIsModalActive] = useState(false)
+
   useEffect(() => {
     getSingleBusiness(
       _id,
@@ -66,23 +68,45 @@ export default function singleBusiness({ _id }) {
             />
         </Head>
       <NavBar />
+
+        <div className={`modal ${isModalActive ? "is-active" : null}`}>
+            <div className="modal-background" onClick={() => setIsModalActive(!isModalActive)}/>
+            <div className="modal-content">
+                <figure className="image modal-menu-image">
+                    <img src={business.menu_url} alt=""/>
+                </figure>
+            </div>
+            <button className="modal-close is-large" aria-label="close" onClick={() => setIsModalActive(!isModalActive)}/>
+        </div>
+
         <div className={"container"}>
             <div className={"box"}>
-                <p className={"title is-size-1"}>{business.business_name}</p>
-                <p className={"subtitle"}>{business.category}</p>
-                <span className={"icon-text mb-2 mr-4"}>
+
+                <div className={"columns"}>
+                    <div className={"column is-narrow is-mobile"}>
+                        <p className={"title is-size-1"}>{business.business_name}</p>
+                        <p className={"subtitle"}>{business.category}</p>
+                        <span className={"icon-text mb-2 mr-4"}>
                     <span className={"icon pb-2"}><i className="fa-solid fa-star" style={{color: "#f1cb29"}}></i></span>
                     <span>{numberOfRatings === 0
                         ? 0
                         : Number(totalRating / numberOfRatings).toFixed(1)}</span>
                 </span>
-                <span className={"icon-text mb-2"}>
+                        <span className={"icon-text mb-2"}>
                     <span className={"icon pb-1"}><i className="fa-solid fa-circle-dot" style={{color: "#01b20d"}}></i></span>
-                    <span>{business.is_active ? "Open" : "This business is currently closed"}</span>
+                    <span>{business.is_active ? "Open" : "Closed"}</span>
                 </span>
+                    </div>
+                    <div className={"column has-text-right is-mobile"}>
+                        <figure className={"image is-128x128 is-inline-block is-clickable"} aria-pressed={"true"} tabIndex={0} onClick={() => setIsModalActive(!isModalActive)}>
+                            <img src={business.menu_url} className={"profile-pic"} alt="the businesses menu"/>
+                        </figure>
+                    </div>
+
+                </div>
 
             </div>
-            <br></br>
+
 
             <div className={"columns"}>
                 <div className={"column is-half"}>
@@ -156,7 +180,7 @@ export default function singleBusiness({ _id }) {
                     setNumberOfRatings={setNumberOfRatings}
                 />
             ) : null}
-            <img src={business.menu_url} alt="the businesses menu"/>
+
             <div>
                 <h1>Reviews:</h1>
                 <ul>
